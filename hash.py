4509fb8409b3e70930bd0688a3f68dcd7b5d68c2
cskings14 <cskings14@gmail.com>
2022-12-21 06:55:36 -0500

    added hash

---
+++ hash.py
@@ -0,0 +1,13 @@
+'''
+Hash tables are used to store key value pairs. 
+If you know the key, the time complexity is O(1) to search and 0(1) to add or delete a key value pair.
+In python, a hash table class is made in the form of a dictionary
+'''
+example = {}
+example['day 1'] = 47
+example['day 2'] = 76
+example['day 3'] = 39
+example['day 4'] = 55
+example['day 5'] = 50
+example['day 6'] = 56
+print(example)
